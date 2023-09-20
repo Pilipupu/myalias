590a8cfb7bb9ec3cbb0f8d05c0d509cf7ae63cd6
Wang,Jing <jing.wang@zstack.io>
2023-09-20 10:45:33 +0800

    init

---
+++ myalias.plugin.zsh
@@ -0,0 +1,7 @@
+alias ll='ls -lhG'
+alias ls='ls -G'
+alias rm='rm -i'
+
+alias synczstack='bash /Users/jingwang/Soft/my_sh/synczstack.sh'
+alias g='bash ~/Soft/my_sh/g.sh'
+alias synczstack='bash /Users/jingwang/Soft/my_sh/synczstack.sh'
