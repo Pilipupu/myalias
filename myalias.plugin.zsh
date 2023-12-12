01a210318b3f3606c68d4ea35c83e9227af7f78c
Wang,Jing <jing.wang@zstack.io>
2023-12-12 17:36:51 +0800

    fix path

--- myalias.plugin.zsh
+++ myalias.plugin.zsh
@@ -3,9 +3,6 @@ alias ls='ls -G'
 alias rm='rm -i'
 alias vi='vim'
 
-alias synczstack='bash $HOME/.local/share/zinit/plugins/Pilipupu---myalias/bash
-/synczstack.sh'
-alias g='bash $HOME/.local/share/zinit/plugins/Pilipupu---myalias/bash
-  7 /g.sh'
-
+alias synczstack='bash $HOME/.local/share/zinit/plugins/Pilipupu---myalias/bash/synczstack.sh'
+alias g='bash $HOME/.local/share/zinit/plugins/Pilipupu---myalias/bash/g.sh'
 alias mt='bash /root/shell/mvt.sh'
