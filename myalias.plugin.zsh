4fa943fd5a702ecc1859d319763090784015ea0e
Wang,Jing <jing.wang@zstack.io>
2024-01-15 16:10:50 +0800

    fix script path

--- myalias.plugin.zsh
+++ myalias.plugin.zsh
@@ -4,7 +4,7 @@ alias rm='rm -i'
 alias vi='vim'
 
 script_path=$(readlink -f "$0")
-BASH_DIR=$(dirname "$script_path")
+BASH_DIR=$(dirname "$script_path")/bash
 alias synczstack="bash $BASH_DIR/synczstack.sh"
 alias g="bash $BASH_DIR/g.sh"
 alias mt='bash /root/shell/mvt.sh'
