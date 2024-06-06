c7099f946cd19dbdc4d218024a4745db1de3f53e
Wang,Jing <jing.wang@zstack.io>
2024-06-06 13:26:55 +0800

    add kubectl alias

--- myalias.plugin.zsh
+++ myalias.plugin.zsh
@@ -9,5 +9,10 @@ alias synczstack="bash $BASH_DIR/synczstack.sh"
 alias g="bash $BASH_DIR/g.sh"
 alias mt='bash /root/shell/mvt.sh'
 alias updatedb='sudo /usr/libexec/locate.updatedb'
+autoload -Uz compinit
+compinit
+alias k=kubectl
+source <(kubectl completion zsh)
+compdef k='kubectl'
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
